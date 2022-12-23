7fa2f10473a82183a68fdeb4d49eeae3e9dfe271
Kiran <84730683+KiranS22@users.noreply.github.com>
2022-12-23 10:21:39 +0000

    Merge conflicts resolved

--- Portfolio_Projects/Full-stack-Kanban/frontend/src/Components/App/App.js
+++ Portfolio_Projects/Full-stack-Kanban/frontend/src/Components/App/App.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from "react";
-import Routing from "./Routing";
+import React, { useEffect, useState } from "react";
 import Routing from "./Routing";
 import "./app.css";
 import { selectTheme } from "../../Redux/features/Slices/Toggler/Toggler";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { logInUser } from "../../Redux/features/Slices/Auth/Auth";
-import { selectToken } from "../../Redux/features/Slices/Auth/Auth";
 import jwt_decode from "jwt-decode";
 import { DragDropContext } from "react-beautiful-dnd";
+import { selectTask } from "../../Redux/features/Slices/Tasks/tasks";
 function App() {
   const mode = useSelector(selectTheme);
   const dispatch = useDispatch();
-  const token = useSelector(selectToken);
+  const tasks = useSelector(selectTask);
+  const [card, setCard] = useState(tasks);
+  const handleDragEnd = () => {
+    console.log("card ", card);
+  };
 
   const getLoggedInUser = async () => {
     try {
@@ -32,7 +35,11 @@ function App() {
   }, []);
 
   return (
-    <DragDropContext onDragEnd={()=>{}}>
+    <DragDropContext
+      onDragEnd={() => {
+        handleDragEnd();
+      }}
+    >
       <div id={`body-bg-${mode}`}>
         <Routing />
       </div>
