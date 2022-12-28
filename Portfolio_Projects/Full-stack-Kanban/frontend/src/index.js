c225f12efe03395c7a5cd58c9df89de82ca60373
Kiran <84730683+KiranS22@users.noreply.github.com>
2022-12-28 17:44:08 +0000

    nav bugs

--- Portfolio_Projects/Full-stack-Kanban/frontend/src/index.js
+++ Portfolio_Projects/Full-stack-Kanban/frontend/src/index.js
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+//import $ from "jquery";
+//import Popper from "popper.js";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "../src/Redux/app/store";
@@ -10,11 +14,9 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  // <React.StrictMode>
   <Provider store={store}>
     <App />
   </Provider>
-  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
